5a35c963aa0daeecdf07c5076d93dd7f4f73d316
Vishal-1756 <126957307+Vishal-1756@users.noreply.github.com>
2023-05-24 09:38:04 +0530

    Update start.py
    Signed-off-by: Vishal-1756 <126957307+Vishal-1756@users.noreply.github.com>

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAJE8GK4EsoLVZC2SW5W5Q-QAkaoN8f_AAL9BQACiy14VGoQxOCDfE1KKQQ")
+            await message.reply_sticker("CAACAgUAAx0CU_72swABAvC1ZG2NUSw_NKyq0v9MtV6yzEjPyP4AAtEIAAJyqolVh1Xc3Al5XoMvBA")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
